d87b3361d2d0694e48ac939e9855f76743c3d15a
Павел Заврицкий <pavelzorro125@yandex.ru>
2025-05-05 00:09:24 +0300

    123

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"df+UemJnBRUVEHymrkxW9QflH+rMvptyQgyXpuSUyaw=\"\n}"
---
+++ .next/static/chunks/app_page_tsx_52e376._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_52e376._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_52e376._.js",
+  "chunks": [
+    "static/chunks/a7abb_next_dist_766dee._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js"
+  ],
+  "source": "dynamic"
+});
