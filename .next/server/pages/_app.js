0b61793e50e501bd1ca0c977695d4f56f4103286
Павел Заврицкий <pavelzorro125@yandex.ru>
2025-06-02 13:47:57 +0300

    Урок 15

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,4 +1,5 @@
 const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
 const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__4faa30._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.6_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
+runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__15cf0af7._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.4.0-canary.23_react_2961b682984212f521c4cc93c3dc7c92/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.4.0-canary.23_react_2961b682984212f521c4cc93c3dc7c92/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
